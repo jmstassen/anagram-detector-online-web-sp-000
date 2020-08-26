80ffb70719b26d0396eda5e19238527a2cf2c63d
John Stassen <john.stassen@gmail.com>
2020-08-26 14:33:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+
+  def initialize(word)
+    anagram = self.new
+    @word = word
+  end
+
+  def match(array_of_possible_anagrams)
+    array_of_possible_anagrams.each do |possible|
+      possible_array = possible.split("")
+      word_array = @word.split("")
+      if possible_array.sort == word_array.sort
+        solution = possible
+      else
+      end
+    end
+    return solution
+  end
